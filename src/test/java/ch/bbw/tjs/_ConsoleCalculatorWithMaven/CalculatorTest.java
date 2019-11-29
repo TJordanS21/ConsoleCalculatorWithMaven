bfa17825754c7d8e89a0bafb585c41a91dd07e17
TJordanS21 <tyler.storz@icloud.com>
2019-11-29 15:16:19 +0100

    Prepare test for Subtraktion

--- src/test/java/ch/bbw/tjs/_ConsoleCalculatorWithMaven/CalculatorTest.java
+++ src/test/java/ch/bbw/tjs/_ConsoleCalculatorWithMaven/CalculatorTest.java
@@ -12,10 +12,17 @@ import org.junit.Test;
  */
 public class CalculatorTest {
 	Calculator testee;
+
 	@Test
 	public void testSummeZweiPositiveIsOk() {
 		testee = new Calculator();
 		assertTrue(testee.summe(10, 25) == 35);
+	}
+
+	@Test
+	public void testSubtractionZweiPositiveIsOk() {
+		testee = new Calculator();
+		assertTrue(testee.subtraktion(25, 10) == 15);
 	}
 
 }
