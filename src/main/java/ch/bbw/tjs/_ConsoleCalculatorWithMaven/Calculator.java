e182ff011fc1852e9cb28cf4ba2cb251ed451bbd
TJordanS21 <tyler.storz@icloud.com>
2020-01-02 21:56:18 +0100

    had to implement test for private Methods

--- src/main/java/ch/bbw/tjs/_ConsoleCalculatorWithMaven/Calculator.java
+++ src/main/java/ch/bbw/tjs/_ConsoleCalculatorWithMaven/Calculator.java
@@ -34,7 +34,7 @@ public class Calculator {
 		return Math.round(100 * startValue * Math.pow((1.0 + zinsRate / 100.0), years)) / 100.0;
 	}
 
-	private double entzinsen(double endValue, double zinsRate, double years) {
+	private double entzinsen(Double endValue, Double zinsRate, Double years) {
 		return Math.round(100 * endValue / Math.pow((1.0 + zinsRate / 100.0), years)) / 100.0;
 	}
 
