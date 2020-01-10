5ee1d1de4a872588a69cdd46af9d470acfca5ecb
TJordanS21 <tyler.storz@icloud.com>
2020-01-10 15:15:31 +0100

    had to change header

--- src/main/java/ch/bbw/tjs/_ConsoleCalculatorWithMaven/WucherzinsException.java
+++ src/main/java/ch/bbw/tjs/_ConsoleCalculatorWithMaven/WucherzinsException.java
@@ -4,7 +4,7 @@ package ch.bbw.tjs._ConsoleCalculatorWithMaven;
  * 
  * @author Tyler Jordan Storz
  * @version 10.01.2020
- *
+ * This Exception was made for an Error where the zins rate is too high.
  */
 public class WucherzinsException extends RuntimeException {
 	private static final long serialVersionUID = -6456952796825385283L;
